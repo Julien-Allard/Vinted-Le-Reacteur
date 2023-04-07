f1b169c552d4a6602dee663a894dbf13d1f28b57
Julien-Allard <julalldev@gmail.com>
2023-04-07 16:19:09 +0200

    Feature/Login rework

--- routes/offer.js
+++ routes/offer.js
@@ -1,177 +1,172 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const User = require("../models/User");
-const Offer = require("../models/Offer");
-const isAuthenticated = require("../middlewares/isAuthenticated");
+const Offer = require('../models/Offer');
+const isAuthenticated = require('../middlewares/isAuthenticated');
 
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+   cloud_name: process.env.CLOUD_NAME,
+   api_key: process.env.API_KEY,
+   api_secret: process.env.API_SECRET,
 });
 
-router.post("/offer/publish", isAuthenticated, async (req, res) => {
-  try {
-    if (
-      req.fields.description.length > 500 ||
-      req.fields.title.length > 50 ||
-      req.fields.price > 100000
-    ) {
-      res.status(401).json({
-        message:
-          "Offer description and/or title is too long, or price is too high",
-      });
-    } else {
+router.post('/offer/publish', isAuthenticated, async (req, res) => {
+   try {
+      if (
+         req.fields.description.length > 500 ||
+         req.fields.title.length > 50 ||
+         req.fields.price > 100000
+      ) {
+         res.status(401).json({
+            message: 'Offer description and/or title is too long, or price is too high',
+         });
+      } else {
+         const pictureToUpload = req.files.picture.path;
+         const result = await cloudinary.uploader.upload(pictureToUpload, {
+            folder: `vinted/offers/`,
+         });
+
+         const newOffer = new Offer({
+            product_name: req.fields.title,
+            product_description: req.fields.description,
+            product_price: req.fields.price,
+            product_details: [
+               {
+                  MARQUE: req.fields.brand,
+               },
+               {
+                  TAILLE: req.fields.size,
+               },
+               {
+                  ETAT: req.fields.condition,
+               },
+               {
+                  COULEUR: req.fields.color,
+               },
+               {
+                  EMPLACEMENT: req.fields.city,
+               },
+            ],
+            owner: req.user,
+            product_image: result.secure_url,
+         });
+
+         await newOffer.save();
+
+         res.status(200).json(newOffer);
+      }
+   } catch (error) {
+      res.status(401).json({ error: { message: error.message } });
+   }
+});
+
+router.put('/offer/modify', isAuthenticated, async (req, res) => {
+   try {
       const pictureToUpload = req.files.picture.path;
       const result = await cloudinary.uploader.upload(pictureToUpload, {
-        folder: `vinted/offers/`,
+         folder: `vinted/offers/`,
       });
 
-      const newOffer = new Offer({
-        product_name: req.fields.title,
-        product_description: req.fields.description,
-        product_price: req.fields.price,
-        product_details: [
-          {
-            MARQUE: req.fields.brand,
-          },
-          {
-            TAILLE: req.fields.size,
-          },
-          {
-            ETAT: req.fields.condition,
-          },
-          {
-            COULEUR: req.fields.color,
-          },
-          {
-            EMPLACEMENT: req.fields.city,
-          },
-        ],
-        owner: req.user,
-        product_image: result.secure_url,
+      const offerToUpdate = await findByIdAndUpdate(req.fields.id, {
+         product_name: req.fields.title,
+         product_description: req.fields.description,
+         product_price: req.fields.price,
+         product_details: [
+            {
+               MARQUE: req.fields.brand,
+            },
+            {
+               TAILLE: req.fields.size,
+            },
+            {
+               ETAT: req.fields.condition,
+            },
+            {
+               COULEUR: req.fields.color,
+            },
+            {
+               EMPLACEMENT: req.fields.city,
+            },
+         ],
+         product_image: result.secure_url,
       });
 
-      await newOffer.save();
-
-      res.status(200).json(newOffer);
-    }
-  } catch (error) {
-    res.status(401).json({ error: { message: error.message } });
-  }
+      if (offerToUpdate) {
+         res.status(200).json({ message: 'Offer successfully updated' });
+      } else {
+         res.status(400).json({ error: "This offer doesn't exist" });
+      }
+   } catch (error) {
+      res.status(401).json({ error: { message: error.message } });
+   }
 });
 
-router.put("/offer/modify", isAuthenticated, async (req, res) => {
-  try {
-    const pictureToUpload = req.files.picture.path;
-    const result = await cloudinary.uploader.upload(pictureToUpload, {
-      folder: `vinted/offers/`,
-    });
+router.get('/offers', async (req, res) => {
+   try {
+      const filters = {};
 
-    const offerToUpdate = await findByIdAndUpdate(req.fields.id, {
-      product_name: req.fields.title,
-      product_description: req.fields.description,
-      product_price: req.fields.price,
-      product_details: [
-        {
-          MARQUE: req.fields.brand,
-        },
-        {
-          TAILLE: req.fields.size,
-        },
-        {
-          ETAT: req.fields.condition,
-        },
-        {
-          COULEUR: req.fields.color,
-        },
-        {
-          EMPLACEMENT: req.fields.city,
-        },
-      ],
-      product_image: result.secure_url,
-    });
-
-    if (offerToUpdate) {
-      res.status(200).json({ message: "Offer successfully updated" });
-    } else {
-      res.status(400).json({ error: "This offer doesn't exist" });
-    }
-  } catch (error) {
-    res.status(401).json({ error: { message: error.message } });
-  }
-});
-
-router.get("/offers", async (req, res) => {
-  try {
-    const filters = {};
-
-    if (req.query.title) {
-      filters.product_name = new RegExp(req.query.title, "i");
-    }
-
-    if (req.query.priceMin || req.query.priceMax) {
-      filters.product_price = {};
-
-      if (req.query.priceMin) {
-        filters.product_price.$gte = Number(req.query.priceMin);
+      if (req.query.title) {
+         filters.product_name = new RegExp(req.query.title, 'i');
       }
 
-      if (req.query.priceMax) {
-        filters.product_price.$lte = Number(req.query.priceMax);
+      if (req.query.priceMin || req.query.priceMax) {
+         filters.product_price = {};
+
+         if (req.query.priceMin) {
+            filters.product_price.$gte = Number(req.query.priceMin);
+         }
+
+         if (req.query.priceMax) {
+            filters.product_price.$lte = Number(req.query.priceMax);
+         }
       }
-    }
 
-    const sortObject = {};
-    if (req.query.sort === "price-desc") {
-      sortObject.product_price = "desc";
-    } else if (req.query.sort === "price-asc") {
-      sortObject.product_price = "asc";
-    }
+      const sortObject = {};
+      if (req.query.sort === 'price-desc') {
+         sortObject.product_price = 'desc';
+      } else if (req.query.sort === 'price-asc') {
+         sortObject.product_price = 'asc';
+      }
 
-    let page;
-    if (Number(req.query.page) < 1) {
-      page = 1;
-    } else {
-      page = Number(req.query.page);
-    }
+      let page;
+      if (Number(req.query.page) < 1) {
+         page = 1;
+      } else {
+         page = Number(req.query.page);
+      }
 
-    let limit = Number(req.query.limit);
+      let limit = Number(req.query.limit);
 
-    const offers = await Offer.find(filters)
-      .populate({
-        path: "owner",
-        select: "account",
-      })
-      .sort(sortObject)
-      .skip((page - 1) * limit)
-      .limit(limit);
+      const offers = await Offer.find(filters)
+         .populate({
+            path: 'owner',
+            select: 'account',
+         })
+         .sort(sortObject)
+         .skip((page - 1) * limit)
+         .limit(limit);
 
-    const count = await Offer.countDocuments(filters);
+      const count = await Offer.countDocuments(filters);
 
-    res.status(200).json({ count: count, offers: offers });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+      res.status(200).json({ count: count, offers: offers });
+   } catch (error) {
+      res.status(400).json({ message: error.message });
+   }
 });
 
-router.get("/offer/:id", async (req, res) => {
-  try {
-    const getOffer = await Offer.findById(req.params.id).populate(
-      "owner",
-      "account"
-    );
-    if (getOffer) {
-      res.status(200).json(getOffer);
-    } else {
-      res.status(400).json({ message: "Wrong ID" });
-    }
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+router.get('/offer/:id', async (req, res) => {
+   try {
+      const getOffer = await Offer.findById(req.params.id).populate('owner', 'account');
+      if (getOffer) {
+         res.status(200).json(getOffer);
+      } else {
+         res.status(400).json({ message: 'Wrong ID' });
+      }
+   } catch (error) {
+      res.status(400).json({ message: error.message });
+   }
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,95 +1,88 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const User = require("../models/User");
-const Offer = require("../models/Offer");
-
 //Outils pour hashage
-const SHA256 = require("crypto-js/sha256");
-const encBase64 = require("crypto-js/enc-base64");
-const uid2 = require("uid2");
+const SHA256 = require('crypto-js/sha256');
+const encBase64 = require('crypto-js/enc-base64');
+const uid2 = require('uid2');
 
-router.post("/user/signup", async (req, res) => {
-  try {
-    const findUserMail = await User.findOne({ email: req.fields.email });
+const User = require('../models/User');
+const ERROR = require('../utils/errorMessages');
 
-    if (findUserMail === null) {
-      if (
-        req.fields.username &&
-        req.fields.username !== "" &&
-        isNaN(req.fields.username)
-      ) {
-        const password = req.fields.password;
-        const salt = uid2(16);
-        const hash = SHA256(password + salt).toString(encBase64);
-        const token = uid2(16);
+router.post('/user/signup', async (req, res) => {
+   try {
+      const findUserMail = await User.findOne({ email: req.fields.email });
 
-        const newUser = new User({
-          email: req.fields.email,
-          account: {
-            username: req.fields.username,
-            phone: req.fields.phone,
-          },
-          token: token,
-          hash: hash,
-          salt: salt,
-        });
+      if (findUserMail === null) {
+         if (req.fields.username && req.fields.username !== '' && isNaN(req.fields.username)) {
+            const password = req.fields.password;
+            const salt = uid2(16);
+            const hash = SHA256(password + salt).toString(encBase64);
+            const token = uid2(16);
 
-        newUser.save();
+            const newUser = new User({
+               email: req.fields.email,
+               account: {
+                  username: req.fields.username,
+                  phone: req.fields.phone,
+               },
+               token: token,
+               hash: hash,
+               salt: salt,
+            });
 
-        const userInfo = {
-          id: newUser.id,
-          token: newUser.token,
-          account: {
-            username: newUser.account.username,
-            phone: newUser.account.phone,
-          },
-        };
+            newUser.save();
 
-        res.status(200).json(userInfo);
+            const userInfo = {
+               id: newUser.id,
+               token: newUser.token,
+               account: {
+                  username: newUser.account.username,
+                  phone: newUser.account.phone,
+               },
+            };
+
+            res.status(200).json(userInfo);
+         } else {
+            res.status(400).json({ message: ERROR.signup });
+         }
       } else {
-        res.status(400).json({ message: "Invalid or missing username" });
+         res.status(400).json({ message: ERROR.signup });
       }
-    } else {
-      res
-        .status(400)
-        .json({ message: "Account with this email already exists" });
-    }
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
+   } catch (error) {
+      res.status(400).json(error.message);
+   }
 });
 
-router.post("/user/login", async (req, res) => {
-  try {
-    const currentUser = await User.findOne({ email: req.fields.email });
-    // console.log(currentUser);
+router.post('/user/login', async (req, res) => {
+   try {
+      const currentUser = await User.findOne({ email: req.fields.email });
 
-    if (currentUser) {
-      const password = req.fields.password;
-      const salt = currentUser.salt;
-      const hash = SHA256(password + salt).toString(encBase64);
+      if (currentUser) {
+         const password = req.fields.password;
+         const salt = currentUser.salt;
+         const hash = SHA256(password + salt).toString(encBase64);
 
-      if (hash === currentUser.hash) {
-        const userInfo = {
-          id: currentUser.id,
-          token: currentUser.token,
-          account: {
-            username: currentUser.account.username,
-            phone: currentUser.account.phone,
-          },
-        };
+         if (hash === currentUser.hash) {
+            const userInfo = {
+               id: currentUser.id,
+               token: currentUser.token,
+               account: {
+                  username: currentUser.account.username,
+                  phone: currentUser.account.phone,
+               },
+            };
 
-        res.status(202).json(userInfo);
+            res.status(202).json(userInfo);
+         } else {
+            res.status(400).json({ message: ERROR.login });
+         }
       } else {
-        res.status(400).json({ message: "Wrong password" });
+         res.status(400).json({ message: ERROR.login });
       }
-    } else {
-      res.status(400).json({ message: "Email is invalid" });
-    }
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
+   } catch (error) {
+      res.status(400).json(error.message);
+   }
 });
 
 module.exports = router;
